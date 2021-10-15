38ac09313f61613b102d3fa439efdf32762f9ca4
Arthur Santana <arthur.alexandre330@gmail.com>
2021-10-15 12:05:59 -0300

    Adicionei os botões - ainda sem lógica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+	const createButtons = () => {
+		var numbers = []
+		for (let i = 1; i < 10; i++) {
+			numbers.push(
+				<button key={i}>{i}</button>
+			)
+		}
+
+		return numbers
+	}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+			<div className="display">
+				<span>(0)</span>0
+			</div>
+
+			<div className="operators">
+				<button>/</button>
+				<button>*</button>
+				<button>+</button>
+				<button>-</button>
+			</div>
+
+			<div className="digits">
+				{createButtons()}
+				<button>0</button>
+				<button>.</button>
+				<button>=</button>
+			</div>
+	  </div>
     </div>
   );
 }
